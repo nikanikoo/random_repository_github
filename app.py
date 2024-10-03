70fb40d0e372e9c59c31e1aa8be0f3924b914f59
Nika <me@nikori.ru>
2024-10-03 22:14:43 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import requests
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    response = requests.get('https://api.github.com/repositories')
+    repositories = response.json()
+
+    random_repo = random.choice(repositories)
+    repo_url = random_repo['html_url']
+    
+    return render_template('index.html', repo_url=repo_url)
+
+if __name__ == '__main__':
+    app.run(debug=True)
